99ba3ec41abd65c11c763e19276ca0a0a0b4a137
Bhagyasree-05 <bhagyasreepalaka@gmail.com>
2026-05-30 14:09:14 +0530

    Add HashMapExample class with basic functionality

---
+++ Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/HashMapExample.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class HashMapExample {
+    public static void main(String[] args) {
+        HashMap<Integer, String> map = new HashMap<>();
+
+        map.put(1, "A");
+        map.put(2, "B");
+
+        System.out.println(map);
+    }
+}
