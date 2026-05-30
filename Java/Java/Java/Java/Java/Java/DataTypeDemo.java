0883f067235a519afaeb7c25a5df95f70e8a49fd
Bhagyasree-05 <bhagyasreepalaka@gmail.com>
2026-05-30 13:54:34 +0530

    Add DataTypeDemo class to demonstrate data types

---
+++ Java/Java/Java/Java/Java/Java/DataTypeDemo.java
@@ -0,0 +1,13 @@
+public class DataTypeDemo {
+    public static void main(String[] args) {
+        int a = 10;
+        float b = 5.5f;
+        char c = 'A';
+        boolean d = true;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
