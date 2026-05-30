5aa61f40f41ef0ef7aeb96ae90be955f38fb2e1c
Bhagyasree-05 <bhagyasreepalaka@gmail.com>
2026-05-30 14:00:58 +0530

    Create ClassObject.java

---
+++ Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/ClassObject.java
@@ -0,0 +1,10 @@
+class Student {
+    String name = "Bhagya";
+}
+
+public class ClassObject {
+    public static void main(String[] args) {
+        Student s = new Student();
+        System.out.println(s.name);
+    }
+}
