8a94ce2bae972ba5bb3ecc2a0dd56a6b9a0461d4
Bhagyasree-05 <bhagyasreepalaka@gmail.com>
2026-05-30 14:14:39 +0530

    Create PatternMatching.java

---
+++ Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/PatternMatching.java
@@ -0,0 +1,10 @@
+public class PatternMatching {
+    public static void main(String[] args) {
+
+        Object obj = "Hello";
+
+        if(obj instanceof String s){
+            System.out.println(s.toUpperCase());
+        }
+    }
+}
