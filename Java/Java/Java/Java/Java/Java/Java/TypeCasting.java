e30969f8c8817752921bd696a5918a980d6b73fd
Bhagyasree-05 <bhagyasreepalaka@gmail.com>
2026-05-30 13:55:04 +0530

    Create TypeCasting.java

---
+++ Java/Java/Java/Java/Java/Java/Java/TypeCasting.java
@@ -0,0 +1,12 @@
+public class TypeCasting {
+    public static void main(String[] args) {
+        int a = 10;
+        double b = a; // implicit
+
+        double x = 9.8;
+        int y = (int)x; // explicit
+
+        System.out.println(b);
+        System.out.println(y);
+    }
+}
