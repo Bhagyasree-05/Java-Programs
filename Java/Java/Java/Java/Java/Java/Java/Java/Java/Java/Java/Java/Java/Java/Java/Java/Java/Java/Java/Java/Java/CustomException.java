3e237795a7e6ace3a74aad768593131f724e38f4
Bhagyasree-05 <bhagyasreepalaka@gmail.com>
2026-05-30 14:04:16 +0530

    Add CustomException class with MyException handling

---
+++ Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/Java/CustomException.java
@@ -0,0 +1,15 @@
+class MyException extends Exception {
+    MyException(String msg){
+        super(msg);
+    }
+}
+
+public class CustomException {
+    public static void main(String[] args) {
+        try {
+            throw new MyException("Custom error occurred");
+        } catch(MyException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
